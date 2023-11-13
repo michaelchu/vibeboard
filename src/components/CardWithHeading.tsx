25e4aaa327a559ea65361f9723ab73225199d428
Michael Chu <mchchu88@gmail.com>
2023-11-13 01:02:36 -0500

    Small UI changes

--- src/components/CardWithHeading.tsx
+++ src/components/CardWithHeading.tsx
@@ -4,34 +4,31 @@ export default function CardWithHeading() {
       {/* Card Headings: Title with Subtitle and Action */}
       <div className="flex flex-col rounded-lg shadow-sm bg-white overflow-hidden dark:text-gray-100 dark:bg-gray-800">
         {/* Card Header */}
-        <div className="py-4 px-5 bg-gray-50 text-center space-y-3 sm:space-y-0 sm:text-left sm:flex sm:justify-between sm:items-center dark:bg-gray-900">
+        <div className="py-4 px-5 bg-gray-50 space-y-3 sm:space-y-0 sm:text-left sm:flex sm:justify-between sm:items-center dark:bg-gray-900/50">
           <div>
             <h3 className="font-semibold mb-1">Sample Keyboard Title</h3>
-            <h4 className="text-sm font-medium text-gray-500 dark:text-gray-400">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </h4>
           </div>
-          <div>
-            <button
-              type="button"
-              className="inline-flex justify-center items-center space-x-2 border font-semibold rounded-lg px-3 py-2 leading-5 text-sm border-gray-200 bg-white text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600 dark:focus:ring-opacity-40 dark:active:border-gray-700"
-            >
-              <svg
-                className="hi-mini hi-star inline-block w-5 h-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <span>{Math.floor(Math.random() * (9999 - 100 + 1)) + 100}</span>
-            </button>
-          </div>
+          {/*<div>*/}
+          {/*  <button*/}
+          {/*    type="button"*/}
+          {/*    className="inline-flex justify-center items-center space-x-2 border font-semibold rounded-lg px-3 py-2 leading-5 text-sm border-gray-200 bg-white text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600 dark:focus:ring-opacity-40 dark:active:border-gray-700"*/}
+          {/*  >*/}
+          {/*    <svg*/}
+          {/*      className="hi-mini hi-star inline-block w-5 h-5"*/}
+          {/*      xmlns="http://www.w3.org/2000/svg"*/}
+          {/*      viewBox="0 0 20 20"*/}
+          {/*      fill="currentColor"*/}
+          {/*      aria-hidden="true"*/}
+          {/*    >*/}
+          {/*      <path*/}
+          {/*        fill-rule="evenodd"*/}
+          {/*        d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"*/}
+          {/*        clip-rule="evenodd"*/}
+          {/*      />*/}
+          {/*    </svg>*/}
+          {/*    <span>{Math.floor(Math.random() * (9999 - 100 + 1)) + 100}</span>*/}
+          {/*  </button>*/}
+          {/*</div>*/}
         </div>
         {/* END Card Header */}
 
@@ -46,8 +43,14 @@ export default function CardWithHeading() {
         {/* Card Body */}
 
         {/* Card Footer */}
-        <div className="py-2 px-5 bg-gray-50 grid grid-cols-3 text-center dark:bg-gray-900">
-          <dl className="py-1 space-y-1">
+        <div className="py-4 px-5 bg-gray-50 space-y-3 sm:space-y-0 sm:text-left sm:flex sm:justify-between sm:items-center dark:bg-gray-900/50">
+          <h4 className="mb-1 text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+            consequat turpis nec diam pretium, in consequat turpis lobortis.
+          </h4>
+        </div>
+        <div className="py-2 px-5 bg-gray-50 grid grid-cols-3 text-center dark:bg-gray-900/70">
+          <dl className="py-2 space-y-1">
             <dt className="text-lg font-bold">15</dt>
             <dd className="text-xs font-semibold uppercase tracking-wider text-gray-500 dark:text-gray-400">
               Projects
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,10 @@ function IndexPage() {
   return (
     <>
       <Heading title={title} />
-      <div className="container xl:max-w-7xl mx-auto p-4 lg:p-8">
+      <div className="container xl:max-w-7xl mx-auto py-10 px-4 lg:p-8">
         <div>
           <Meta />
-          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-10">
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-10">
             <CardWithHeading />
             <CardWithHeading />
             <CardWithHeading />
