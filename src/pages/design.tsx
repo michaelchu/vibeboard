b7fd5efb426549090345b9b3f5e34ca11392d913
Michael Chu <mchchu88@gmail.com>
2023-11-14 01:09:19 -0500

    Modify stub for design page 2

--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Meta from "../components/Meta";
 import { requireAuth } from "../util/auth.jsx";
-import Dropdown from "../components/Dropdown.tsx";
 import Keyboard from "../components/Keyboard/Keyboard.tsx";
 import { mergeArraysByKey } from "../util/helpers.ts";
 import { mac_os_65 } from "../components/Keyboard/layouts/mac_os_65.ts";
@@ -14,38 +13,37 @@ import { ArrowUturnRightIcon } from "@heroicons/react/24/solid";
 
 const filters = [
   {
-    id: "color",
-    name: "Color",
+    id: "layout",
+    name: "Layout",
     options: [
-      { value: "white", label: "White" },
-      { value: "beige", label: "Beige" },
-      { value: "blue", label: "Blue" },
-      { value: "brown", label: "Brown" },
+      { value: "40", label: "40%" },
+      { value: "40_ortho", label: "40% Ortho" },
+      { value: "50_ortho", label: "50% Ortho" },
+      { value: "60", label: "60%" },
+      { value: "65", label: "65%" },
+      { value: "75", label: "75%" },
+      { value: "80", label: "80%" },
+      { value: "100", label: "90%" },
+    ],
+  },
+  {
+    id: "case_style",
+    name: "Case Style",
+    options: [
+      { value: "angular", label: "Angular" },
+      { value: "rounded", label: "Rounded" },
+    ],
+  },
+  {
+    id: "case_color",
+    name: "Case Color",
+    options: [
+      { value: "red", label: "Red" },
+      { value: "pink", label: "Pink" },
+      { value: "yellow", label: "Yellow" },
       { value: "green", label: "Green" },
+      { value: "blue", label: "Blue" },
       { value: "purple", label: "Purple" },
-    ],
-  },
-  {
-    id: "category",
-    name: "Category",
-    options: [
-      { value: "new-arrivals", label: "All New Arrivals" },
-      { value: "tees", label: "Tees" },
-      { value: "crewnecks", label: "Crewnecks" },
-      { value: "sweatshirts", label: "Sweatshirts" },
-      { value: "pants-shorts", label: "Pants & Shorts" },
-    ],
-  },
-  {
-    id: "sizes",
-    name: "Sizes",
-    options: [
-      { value: "xs", label: "XS" },
-      { value: "s", label: "S" },
-      { value: "m", label: "M" },
-      { value: "l", label: "L" },
-      { value: "xl", label: "XL" },
-      { value: "2xl", label: "2XL" },
     ],
   },
 ];
@@ -59,7 +57,6 @@ function DesignPage() {
   const { data: key_colors, status } = useKeyboardByTheme(
     "ca5d93da-28ab-4892-803d-13dad50b9a22",
   );
-  // @ts-ignore
   return (
     <>
       <div className="bg-gray-50 dark:bg-gray-800/50">
@@ -127,14 +124,14 @@ function DesignPage() {
                     leaveFrom="translate-x-0"
                     leaveTo="translate-x-full"
                   >
-                    <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+                    <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto py-4 pb-6 shadow-xl bg-gray-800">
                       <div className="flex items-center justify-between px-4">
-                        <h2 className="text-lg font-medium text-gray-900">
+                        <h2 className="text-lg font-medium text-gray-200">
                           Filters
                         </h2>
                         <button
                           type="button"
-                          className="-mr-2 flex h-10 w-10 items-center justify-center p-2 text-gray-400 hover:text-gray-500"
+                          className="-mr-2 flex h-10 w-10 items-center justify-center p-2 text-gray-200 hover:text-gray-300"
                           onClick={() => setMobileFiltersOpen(false)}
                         >
                           <span className="sr-only">Close menu</span>
@@ -148,13 +145,13 @@ function DesignPage() {
                           <Disclosure
                             as="div"
                             key={section.name}
-                            className="border-t border-gray-200 pb-4 pt-4"
+                            className="border-t border-gray-700 pb-4 pt-4"
                           >
                             {({ open }) => (
                               <fieldset>
                                 <legend className="w-full px-2">
                                   <Disclosure.Button className="flex w-full items-center justify-between p-2 text-gray-400 hover:text-gray-500">
-                                    <span className="text-sm font-medium text-gray-900">
+                                    <span className="text-sm font-medium text-gray-300">
                                       {section.name}
                                     </span>
                                     <span className="ml-6 flex h-7 items-center">
@@ -181,11 +178,11 @@ function DesignPage() {
                                             name={`${section.id}[]`}
                                             defaultValue={option.value}
                                             type="checkbox"
-                                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                            className="h-4 w-4 rounded border-gray-200 bg-gray-300 text-blue-600 focus:ring-blue-500"
                                           />
                                           <label
                                             htmlFor={`${section.id}-${optionIdx}-mobile`}
-                                            className="ml-3 text-sm text-gray-500"
+                                            className="ml-3 text-sm text-gray-400"
                                           >
                                             {option.label}
                                           </label>
@@ -214,11 +211,11 @@ function DesignPage() {
                   className="inline-flex items-center lg:hidden"
                   onClick={() => setMobileFiltersOpen(true)}
                 >
-                  <span className="text-sm font-medium text-gray-700">
+                  <span className="text-sm font-medium text-gray-300">
                     Filters
                   </span>
                   <PlusIcon
-                    className="ml-1 h-5 w-5 flex-shrink-0 text-gray-400"
+                    className="ml-1 h-5 w-5 flex-shrink-0 text-gray-300"
                     aria-hidden="true"
                   />
                 </button>
@@ -264,7 +261,7 @@ function DesignPage() {
               </aside>
 
               {/* Product grid */}
-              <div className="mt-6 lg:col-span-2 lg:mt-0 xl:col-span-3">
+              <div className="mt-10 lg:col-span-2 lg:mt-0 xl:col-span-3">
                 <div className="flex items-center justify-center text-gray-400 w-full h-full">
                   <div>
                     {status === "idle" || status === "loading" ? (
