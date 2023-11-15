67403eb54eda5813d08b5305411096423aada692
Michael Chu <michael.chu@myeatclub.com>
2023-11-14 22:49:00 -0500

    Fix merge issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useAuth } from "../util/auth.jsx";
 import { classNames } from "../util/helpers.js";
 import {
