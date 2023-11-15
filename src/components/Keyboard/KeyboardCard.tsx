5ecc22406b9c3a1236f52939deab1487462dd025
Jeffrey Chu <141854177+JeffreyChuCPA@users.noreply.github.com>
2023-11-14 23:01:37 -0500

    Sm size button fixed via config file (#45)
    Co-authored-by: Michael Chu <mchchu88@gmail.com>
    Co-authored-by: Michael Chu <michael.chu@myeatclub.com>

--- src/components/Keyboard/KeyboardCard.tsx
+++ src/components/Keyboard/KeyboardCard.tsx
@@ -23,13 +23,13 @@ export default function KeyboardCard({ keyboard }) {
       {/* Card Headings: Title with Subtitle and Action */}
       <div className="flex flex-col rounded-lg shadow-sm bg-white overflow-hidden dark:text-gray-101 dark:bg-gray-800">
         {/* Card Header */}
-        <div className="py-4 px-5 bg-gray-50 space-y-3 sm:space-y-0 sm:text-left sm:flex sm:justify-between sm:items-center dark:bg-gray-800/70">
+        <div className="py-4 px-5 bg-gray-50 text-left flex justify-between items-center dark:bg-gray-800/70">
           <div>
             <h2 className="font-semibold text-lg mb-1">
               {lorem.generateWords(2)}
             </h2>
           </div>
-          <div className={"hidden md:block"}>
+          <div>
             <button
               type="button"
               className="inline-flex justify-center items-center space-x-3 border font-semibold rounded-lg px-3 py-2 leading-5 text-sm border-gray-200 bg-white text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600 dark:focus:ring-opacity-40 dark:active:border-gray-700"
