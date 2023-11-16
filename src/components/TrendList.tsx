45076da0479dc707bbe464aa328130142eb9076f
Michael Chu <mchchu88@gmail.com>
2023-11-16 16:39:49 -0500

    Minor UI tweaks for TrendList.tsx

--- src/components/TrendList.tsx
+++ src/components/TrendList.tsx
@@ -15,14 +15,14 @@ export default function TrendList({
 }) {
   return (
     <>
-      <nav className="overflow-hidden rounded-lg border border-gray-200 bg-white dark:divide-gray-700 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100">
-        <div className={"px-4 py-2 font-semibold"}>{title}</div>
+      <nav className="p-5 overflow-hidden rounded-lg border border-gray-200 bg-white dark:divide-gray-700 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100">
+        <div className={"font-semibold"}>{title}</div>
         {list.map((item: listItemProps, idx: number) => {
           return (
             <Link
               key={idx}
               to={item.href}
-              className="flex items-center justify-between px-4 py-3 text-gray-700 hover:bg-gray-50 hover:text-gray-700 active:bg-white dark:text-gray-200 dark:hover:bg-gray-800/50 dark:active:bg-gray-900"
+              className="flex items-center justify-between py-3 text-gray-700 hover:bg-gray-50 hover:text-gray-700 active:bg-white dark:text-gray-200 dark:hover:bg-gray-800/50 dark:active:bg-gray-900"
             >
               <div className="flex items-center space-x-4">
                 {placeholder ? (
