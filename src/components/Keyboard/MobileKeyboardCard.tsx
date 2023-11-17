632fb6c581162faf5495a278871940551a6ac217
Michael Chu <mchchu88@gmail.com>
2023-11-17 01:56:30 -0500

    Increase mobile action button size

--- src/components/Keyboard/MobileKeyboardCard.tsx
+++ src/components/Keyboard/MobileKeyboardCard.tsx
@@ -57,20 +57,20 @@ export default function MobileKeyboardCard({
             type="button"
             className="inline-flex items-center rounded-md justify-center space-x-2 border border-gray-200 bg-white px-2 py-1 text-sm font-semibold leading-5 text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600 dark:focus:ring-opacity-40 dark:active:border-gray-700"
           >
-            <HeartIcon className={"h-4 w-4"} />
+            <HeartIcon className={"h-5 w-5"} />
           </button>
           <button
             type="button"
             className="inline-flex items-center rounded-md justify-center space-x-2 border border-gray-200 bg-white px-2 py-1 text-sm font-semibold leading-5 text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600 dark:focus:ring-opacity-40 dark:active:border-gray-700"
           >
-            <PlusIcon className={"h-4 w-4"} />
+            <PlusIcon className={"h-5 w-5"} />
           </button>
         </div>
         <button
           type="button"
           className="inline-flex items-center rounded-md justify-center space-x-2 border border-gray-200 bg-white px-2 py-1 text-sm font-semibold leading-5 text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600 dark:focus:ring-opacity-40 dark:active:border-gray-700"
         >
-          <ShareIcon className={"h-4 w-4"} />
+          <ShareIcon className={"h-5 w-5"} />
         </button>
       </div>
     </>
